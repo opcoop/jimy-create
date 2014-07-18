97e84052358ddca5b86ba9205daf771f5981807e
Niv Sardi <xaiki@evilgiggle.com>
2014-07-18 13:51:49 -0300

    add tapa
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- app.js
+++ app.js
@@ -4,19 +4,34 @@ $(function() {
         easing : 'ease-in-out',
         centered: true,
         folds : 2,
-        folddirection : ['left','bottom','right'],
+            folddirection : ['right', 'bottom'],
         onEndUnfolding: function () {
-            var map = L.map('map').setView([-55.505, -51.09], 4);
+                var map = L.map('map')
+                            .setView([-40.17887331434695, -63.896484375], 4)
+                            .locate({
+                                    setView: true
 
-            // add MapQuest tile layer, must give proper OpenStreetMap attribution according to MapQuest terms
-            L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a>',
-                opacity: 0.8
-            }).addTo(map);
+                            });
+
+                map.on('locationfound', function(LocationEvent) {
+	                /*Fired when geolocation (using the locate method) went successfully. */
+                        console.log ('locationfound', LocationEvent);
+                });
+                map.on('locationerror', function(ErrorEvent) {
+                        console.log ('locationerror', ErrorEvent);
+                });
+
+                // add MapQuest tile layer, must give proper OpenStreetMap attribution according to MapQuest terms
+                L.tileLayer('http://otile4.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', {
+                        attribution: '&copy; <a href="www.openstreetmap.org/copyright">OpenStreetMap</a>',
+                        opacity: 0.8
+                }).addTo(map);
+                window.map = map;
         }
 
     });
     $('#tapa').on('click', function () {
         pfold.unfold();
     });
+
 });
